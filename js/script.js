fbc991ae5296c24e24026fef24d2b85ddeddd681
Denise Molina <denimoli2000@gmail.com>
2022-07-10 03:31:23 -0300

    primer commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let edad = parseInt(prompt("Ingresar edad"));
+
+for(i=1;i<4;i++){
+    let resultado = edad + i;
+   // alert (edad+ "+"+i+"="+resultado);
+    alert ('si le sumamos un año más a tu edad actual ésta será de...'+ (i + edad))
+}
