3d9693da9eb0ab736a72ac3949e39faa2668a275
Denise Molina <denimoli2000@gmail.com>
2022-08-11 03:30:19 -0300

    commit nuevo

--- js/index.js
+++ js/index.js
@@ -2,9 +2,9 @@
 let readMore_btn = document.getElementById('readMore_btn');
 let hideText = document.getElementById('hideText');
 
-readMore_btn.addEventListener('click', toggleText);
+readMore_btn.addEventListener('click', toggle);
 
-function toggleText() {
+function toggle() {
   hideText.classList.toggle('showText');
 
   if(hideText.classList.contains('showText')) {
@@ -15,5 +15,16 @@ function toggleText() {
   }
 }
 
-//pelicula2
+//proximamente
+/*
+let nuevosEstrenos = document.createElement('h5');
+nuevosEstrenos.innerHTML = '<h5>Proximamente</h5>';
+document.body.append(nuevosEstrenos);
+*/
 
+const nuevosEstrenos = document.querySelector("h5");
+
+const h5 = document.createElement("h5");
+h5.innerHTML = "Proximamente en estrenarse"
+
+nuevosEstrenos.appendChild(h5)
